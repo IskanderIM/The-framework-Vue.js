70a298c9345c6788682b81d61dab4eae1e949079
IskanderIM <isk787739@gmail.com>
2024-07-12 12:53:18 +0500

    Homework for lesson 10 new

---
+++ homework5/task2-vue-project/src/components/ProductDetails.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in products" :key="item.id">
+        <h2>{{ item.name }}</h2>
+        <h2><span>{{ item.selected }}</span>{{ item.price }}</h2>
+        <input type="checkbox" name="available" id="available" v-model="item.available">
+        <p v-if="item.available === true">Available</p>
+        <p v-else>Out of stock</p>
+          <span>Пересчитать цену</span>
+        <select @change="toggleDescription(item.id, item)" v-model="item.selected">
+          <option>₽</option>
+          <option>$</option>
+          <option>€</option>
+        </select>
+        <!-- <p>{{ item.result }}</p>
+        <p>{{ item.selected }}</p> -->
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetails',
+
+  data () {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'Product 1',
+          price: 300,
+          available: true,
+          selected: '',
+          result: 0
+        },
+        {
+          id: 2,
+          name: 'Product 2',
+          price: 600,
+          available: false,
+          selected: '',
+          result: 0
+        },
+        {
+          id: 3,
+          name: 'Product 3',
+          price: 200,
+          available: true,
+          selected: '',
+          result: 0
+        }
+      ]
+    }
+  },
+
+  mounted () {
+  },
+
+  methods: {
+    toggleDescription (id, item) {
+      // console.log(id)
+      // console.log(item)
+      // console.log(item.price)
+      // console.log(item.selected)
+      item.result = 0
+      if (id || item.selected === '₽') {
+        item.result = 0
+        item.result = Number(item.price)
+        console.log(item.result)
+      } else if (id || item.selected === '$') {
+        item.result = 0
+        item.result = Number(item.price) * Number(87.51)
+        console.log(item.result)
+      } else if (id || item.selected === '€') {
+        item.result = 0
+        item.result = Number(item.price) * Number(95.10)
+      }
+    },
+    ruble () {
+      console.log(this)
+      console.log(this.selected)
+      this.result = Number(this.$parent.item.price)
+    },
+    dollar () {
+      this.result = Number(this.item.price) * Number(87.51)
+    },
+    euro () {
+      this.result = Number(this.price) * Number(95.10)
+    },
+    formattedPrice (selected = '₽') {
+      switch (selected) {
+        case '₽':
+          this.ruble()
+          break
+        case '$':
+          this.dollar()
+          break
+        case '€':
+          this.euro()
+          break
+      }
+    }
+  }
+}
+</script>
